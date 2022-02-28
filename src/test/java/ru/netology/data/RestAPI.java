66df3b9ac04ba1735989d04c37ce9c8a5a263fac
Ekaterina Shilova <ekaterinalstmpr@gmail.com>
2022-02-28 12:40:11 +0300

    fix

--- src/test/java/ru/netology/data/RestAPI.java
+++ src/test/java/ru/netology/data/RestAPI.java
@@ -4,10 +4,8 @@ import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.LogDetail;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
-import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
-import static io.restassured.RestAssured.requestSpecification;
 
 public class RestAPI {
     static String appUrl = System.getProperty("app.url");
